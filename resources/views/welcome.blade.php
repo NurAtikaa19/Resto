d5f268875a29f77671361f12d1c62f2e1ea1bac7
NurAtika19 <nuratika190103@gimail.com>
2024-06-08 17:06:59 +0700

    menambahkan Templating

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,7 @@
+@extends('layout.master')
+@section('titel')
+   Selamat Datang Di Dealmeal
+@endsection
+@section('content')
+<img src="{{ asset('image/Resto.jpg') }}" alt="Resto" width="200" height="150">
+@endsection
